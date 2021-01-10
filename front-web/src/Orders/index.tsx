1379cf19c96283eee81a180c8dca52cdd402d331
Manoel Braz Maciel <manoelbraz.maciel@gmail.com>
2021-01-09 22:12:47 -0300

    Add ... Routes and StepsHeaders!

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,14 @@
+import StepsHeaders from "./StepsHeaders";
+import "./styles.css";
+
+function Orders() {
+    return (
+        <>
+            <div className="orders-container">
+                <StepsHeaders />
+            </div>
+        </>
+    );
+}
+
+export default Orders;
