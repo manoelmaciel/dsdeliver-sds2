0ba4098b292ccfe902352cf3eb48294e5b962897
manoelmaciel <manoelbraz.maciel@gamil.com>
2021-01-18 00:41:28 -0300

    Não ... Está funcionando. Infelizmente!

--- front-web/src/Orders/ProductsList.tsx
+++ front-web/src/Orders/ProductsList.tsx
@@ -13,7 +13,9 @@ function ProductsList({ products, onSelectProduct, selectedProducts }: Props) {
         <div className="orders-list-container">
             <div className="orders-list-items">
                 {products.map(product => (
-                    <ProductCard key={product.id} product={product} onSelectProduct={onSelectProduct} isSelected={checkIsSelected(selectedProducts, product)} />
+                    <ProductCard key={product.id} product={product} 
+                        onSelectProduct={onSelectProduct} 
+                        isSelected={checkIsSelected(selectedProducts, product)} />
                 ))}
             </div>
         </div>
--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { OrderPayLoad } from "./Orders/types";
 
-// const API_URL = process.env.REACT_APP_API_URL;
 const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
